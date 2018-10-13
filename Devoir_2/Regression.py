7ca59751e50bb5c9f9ada6f1e4ec4eb7ea85c005
Philippe <phlip.marcotte@gmail.com>
2018-10-13 13:47:44 -0400

    2.6 complete

--- Devoir_2/Regression.py
+++ Devoir_2/Regression.py
@@ -1,36 +1,41 @@
 import numpy as np
-
+import math
 class RegressionGradient:
-    def __init__(self, step_size=0.5, lambda_l2=1, nb_steps=1000):
+    def __init__(self, step_size=0.5, lambda_l2=1, epochs=100):
         self.step_size = step_size
         self.lambda_l2 = lambda_l2
-        self.nb_steps = nb_steps
+        self.epochs = epochs
 
     def compute_cost(self, error):
-        return error.T.dot(error) + self.lambda_l2 * self.theta.T.dot(self.theta)
+        return ((error.T.dot(error) + self.lambda_l2 * self.theta.T.dot(self.theta))).squeeze()
 
     def gradient_step(self):
-        error = self.train_inputs.dot(self.theta) - self.train_targets
+        error = self.train_inputs.dot(self.theta).reshape((-1,1)) - self.train_targets
         cost = self.compute_cost(error)
-        gradient = 2 * self.train_inputs.T.dot(error) / self.train_size
+        gradient = self.train_inputs.T.dot(error)
 
-        self.theta = self.theta - self.step_size * gradient + 2 * self.lambda_l2 * self.theta
+        self.theta = self.theta - self.step_size / self.train_size * (gradient + self.lambda_l2 * self.theta)
         return cost
 
     def train(self, train_inputs, train_targets):
-        self.train_inputs = np.vstack((np.ones(train_inputs.shape[0]), train_inputs)).T
+        #self.train_inputs = np.vstack((np.ones(train_inputs.shape[0]), train_inputs)).T
+        self.train_inputs = np.insert(train_inputs, 0, 1, axis=1)
         self.train_size = self.train_inputs.shape[0]
 
         self.train_targets = train_targets
 
-        self.theta = np.random.uniform(size=(train_inputs.shape[1] + 1)) if train_inputs.ndim > 1 else np.random.uniform(size=(2))
+        self.theta = np.random.uniform(size=(train_inputs.shape[1] + 1, 1)) if train_inputs.ndim > 1 else np.random.uniform(size=(2, 1))
 
-        for i in range(self.nb_steps):
+        self.i = 0
+        theta_isnan = False
+        for self.i in range(self.epochs):
             cost = self.gradient_step()
-            print("Iteration : {}, cost : {}".format(i, cost))
+            theta_isnan = np.any(np.isnan(self.theta))
+
+        print("Iteration : {}, cost : {}".format(self.i + 1, cost))
 
         return self.theta
 
     def predict(self, test_inputs):
-        test_inputs = np.vstack((np.ones(test_inputs.shape[0]), test_inputs)).T
-        return test_inputs.dot(self.theta)
+        test_inputs = np.insert(test_inputs, 0, 1, axis=1)
+        return test_inputs.dot(self.theta).reshape((-1, 1))
--- Devoir_2/TestRegression.py
+++ Devoir_2/TestRegression.py
@@ -1,30 +1,64 @@
 from Regression import RegressionGradient
 import matplotlib.pyplot as plt
 import numpy as np
-
+np.seterr(all='raise')
 def h(x):
     return np.sin(x) + 0.3 * x - 1
 
-np.random.seed(100)
-X = np.random.uniform(-5, 5, 15)
-Y = h(X)
+dn = np.random.uniform(-5, 5, 15).reshape((-1 ,1))
+dn = np.sort(dn, axis=0)
+dn_labels = h(dn)
 
-regression_gradient = RegressionGradient(step_size=0.01, lambda_l2=0)
-regression_gradient_medLambda = RegressionGradient(step_size=0.001, lambda_l2=0.0001)
-#regression_gradient_highLambda = RegressionGradient(step_size=0.001, lambda_l2=0.1)
+dtest = np.random.uniform(-5, 5, 100).reshape((-1,1))
+dtest= np.sort(dtest, axis=0)
+dtest_labels = h(dtest)
 
-regression_gradient.train(X, Y)
-regression_gradient_medLambda.train(X, Y)
-#regression_gradient_highLambda.train(X, Y)
 
-xgrid = np.arange(-10, 10)
+def phi(X, l=1):
+    phi = []
+    for x in X:
+        powers = []
+        for n in x:
+            powers.append([n ** power for power in range(1, l + 1)])
+        powers = np.array(powers)
 
-figure = plt.figure()
-ax = figure.add_subplot(1,1,1)
-ax.set_xticks(xgrid)
-ax.scatter(X, Y)
-ax.plot(X, regression_gradient.predict(X), color='orange')
-ax.plot(X, regression_gradient_medLambda.predict(X), color='red')
-#ax.plot(X, regression_gradient_highLambda.predict(X), color='green')
-ax.legend(["h(x)", "regression_gradient (lambda=0)", "regression_gradient (lambda=0.5)"])
+        phi_row = []
+        for i in range(powers.shape[0]):
+            current_row = powers[i]
+            phi_row.extend(current_row)
+            for j in range(i + 1, powers.shape[0]):
+                second_row = powers[j]
+                for k in range(l):
+                    for m in range(l - k - 1):
+                        phi_row.append(current_row[k] * second_row[m])
+        phi.append(phi_row)
+    return np.array(phi)
+
+
+powers = [20]
+
+
+def min_max_normalization(dn, high=1.0, low=-1.0):
+    mins = np.min(dn, axis=0)
+    maxs = np.max(dn, axis=0)
+    rng = maxs - mins
+    return high - (((high - low) * (maxs - dn)) / rng)
+
+
+legend = []
+for power in powers:
+    phi_dn = phi(dn, power)
+    phi_dn = min_max_normalization(phi_dn)
+    phi_dtest = phi(dtest, power)
+    phi_dtest = min_max_normalization(phi_dtest)
+    regression_gradient = RegressionGradient(step_size=0.01, lambda_l2=0.01, epochs=100000)
+    regression_gradient.train(phi_dn, dn_labels)
+    predictions = regression_gradient.predict(phi_dtest)
+    plt.plot(dtest, predictions)
+    legend.append("l={}".format(power))
+
+plt.scatter(dtest, dtest_labels)
+legend.append("dtest")
+plt.legend(legend)
+plt.title("Prédictions sur Dtest avec ")
 plt.show()
